e4453d616374958d70b7d372484f04e9e6f9296e
Dmitri Maksimov <dmitri.maksimov@bytelife.com>
2017-08-22 17:29:25 +0300

    Simplified HelloActor.createReceive.

--- src/main/kotlin/org/mcdimus/Main.kt
+++ src/main/kotlin/org/mcdimus/Main.kt
@@ -4,8 +4,9 @@ import akka.actor.AbstractActor
 import akka.actor.ActorRef
 import akka.actor.ActorSystem
 import akka.actor.Props
+import org.mcdimus.HelloActor.Goodbye
 import org.mcdimus.HelloActor.Greeting
-import org.mcdimus.HelloActor.SayGoodbye
+import org.mcdimus.extensions.match
 
 class Main {
 
@@ -16,31 +17,35 @@ class Main {
       val system = ActorSystem.create("MyActorSystem")
       val helloActor = system.actorOf(Props.create(HelloActor::class.java), "first-actor")
       helloActor.tell(Greeting("Hello", "Dmitri"), ActorRef.noSender())
-      helloActor.tell(SayGoodbye("Bye"), ActorRef.noSender())
-      
+      helloActor.tell(Goodbye("Bye"), ActorRef.noSender())
+
       system.terminate()
     }
   }
 
 }
 
-class HelloActor: AbstractActor() {
+class HelloActor : AbstractActor() {
 
   data class Greeting(val msg: String, val name: String)
-  data class SayGoodbye(val msg:String)
+  data class Goodbye(val msg: String)
 
   override fun createReceive(): Receive {
     return receiveBuilder()
-        .match(Greeting::class.java) {
-          println("Greetings")
-          println("it.name = ${it.name}")
-          println("it.msg = ${it.msg}")
-        }
-        .match(SayGoodbye::class.java) {
-          println("Goodbye")
-          println("it.msg = ${it.msg}")
-        }
+        .match(this::onGreeting)
+        .match(this::onGoodbye)
         .build()
+  }
+
+  private fun onGreeting(it: Greeting) {
+    println("Greetings")
+    println("it.name = ${it.name}")
+    println("it.msg = ${it.msg}")
+  }
+
+  private fun onGoodbye(it: Goodbye) {
+    println("Goodbye")
+    println("it.msg = ${it.msg}")
   }
 
 }
---
+++ src/main/kotlin/org/mcdimus/extensions/ReceiveBuilderX.kt
@@ -0,0 +1,15 @@
+package org.mcdimus.extensions
+
+import akka.japi.pf.ReceiveBuilder
+import kotlin.reflect.KClass
+
+/**
+ * @author Dmitri Maksimov
+ */
+fun <T : Any> ReceiveBuilder.match(klass: KClass<T>, block: (T) -> Unit): ReceiveBuilder {
+  return this.match(klass.java, block)
+}
+
+inline fun <reified T : Any> ReceiveBuilder.match(noinline block: (T) -> Unit): ReceiveBuilder {
+  return this.match(T::class.java, block)
+}
